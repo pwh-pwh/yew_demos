e34d128a7ba1db05ecd7e6479f55af76e4d14c10
pwh-pwh <plmoknpwh@163.com>
2023-05-04 20:22:01 +0800

    添加NodeRef和use_effect的使用

--- yewdux_demo/src/components/todoInput.rs
+++ yewdux_demo/src/components/todoInput.rs
@@ -1,40 +1,50 @@
 use js_sys::Date;
 use web_sys::HtmlInputElement;
-use yew::{AttrValue, Callback, function_component, html, Html, props, TargetCast, use_state};
-use yew::html::onchange;
+use yew::{function_component, html, Html, use_effect, use_node_ref};
+use gloo_console::log;
 use crate::components::todo::TodoProps;
 use yewdux::prelude::*;
 use crate::store::reducer_store::TodoAction::Add;
 use crate::store::reducer_store::TodoPropsVec;
 
 
+
 #[function_component]
 pub fn TodoInput() -> Html {
-    let textState = use_state(String::default);
-    let changeTextHandler = {
-        let textState = textState.clone();
-        Callback::from(move |e:onchange::Event| {
-            textState.set(e.target_unchecked_into::<HtmlInputElement>().value())
-        })
-    };
+    let input_ref = use_node_ref();
+
+    {
+        let input_ref = input_ref.clone();
+        use_effect(move||{
+            let input_ref = input_ref.cast::<HtmlInputElement>().expect("can not fc");
+            _ = input_ref.focus();
+        });
+    }
+
+
     let dispatch = Dispatch::<TodoPropsVec>::new();
     let addTodoHandler = {
-        let textState = textState.clone();
-        let text:AttrValue = (*textState).clone().into();
+        let input_ref = input_ref.clone();
+        // log!(input_ref);
+        log!("my log");
+
+//误区，组件还没渲染
+        /*let mut input = input_ref.cast::<HtmlInputElement>().expect("can not fc");
+        let text = input.value();*/
          move|_| {
+             let mut input = input_ref.cast::<HtmlInputElement>().expect("can not fc");
              dispatch.apply(Add(TodoProps{
                  id: Date::new_0().get_time() as i64,
-                 text: text.clone(),
+                 text: input.value().into(),
                  isFinished: false,
              }));
-            textState.set("".into());
+             input.set_value("");
         }
     };
-    let text = (*textState).clone();
     html! {
         <>
         <div class="input-group mb-3">
-            <input type="text" placeholder="请输入代办事项" onchange={changeTextHandler} value={text}/>
+            <input type="text" placeholder="请输入代办事项" ref={input_ref.clone()}/>
             <button class="form-control" onclick={addTodoHandler}>{"添加"}</button>
         </div>
         </>
